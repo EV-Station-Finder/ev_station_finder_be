7263dd1515ed0c5970972a995db475d9e2e0ff93
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-08-22 12:31:51 -0600

    Created index_spec feature test
    Co-authored-by: Diana Buffone 
    <71909590+Diana20920@users.noreply.github.com>

---
+++ spec/requests/api/v1/stations/index_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe "Search for stations by location" do
+  describe "Happy Path" do
+    it "Enpoint exists and has attributes" do
+      location = "Denver, CO"
+      get "/api/v1/stations?location=#{location}"  
+      expect(response).to be_successful
+      
+      body = JSON.parse(response.body, symbolize_names:true)
+      expect(body).to have_key(:data)
+      expect(body[:data]).to have_key(:id)
+      expect(body[:data]).to have_key(:type)
+      expect(body[:data]).to have_key(:attributes)
+      expect(body[:data][:id]).to eq(nil)
+      expect(body[:data][:type]).to be_a String
+      expect(body[:data][:attributes]).to be_an Array
+      expect(body[:data][:attributes].size).to be < 21
+      
+      station_1 = body[:data][:attributes][0]
+      
+      expect(station_1).to be_a Hash
+      expect(station_1.size).to eq(9)
+      expect(station_1).to have_key(:name)
+      expect(station_1).to have_key(:distance)
+      expect(station_1).to have_key(:status)
+      expect(station_1).to have_key(:hours)
+      expect(station_1).to have_key(:ev_network)
+      expect(station_1).to have_key(:street_address)
+      expect(station_1).to have_key(:city)
+      expect(station_1).to have_key(:state)
+      expect(station_1).to have_key(:zip_code)     
+    end
+  end
+  
+  describe "Sad Path/Edge Cases" do
+    it "location is not provided" do
+      get "/api/v1/stations"  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      body = JSON.parse(response.body, symbolize_names:true)
+      expect(body).to have_key(:errors)
+      expect(body[:errors]).to eq("A valid location must be provided")
+    end
+    
+    it "location is blank" do
+      location = " "
+      get "/api/v1/stations?location=#{location}"  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      body = JSON.parse(response.body, symbolize_names:true)
+      expect(body).to have_key(:errors)
+      expect(body[:errors]).to eq("A valid location must be provided")
+    end
+    
+    it "location is invalid" do
+      location = "oghkhhohoiho79808707"
+      get "/api/v1/stations?location=#{location}"  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      body = JSON.parse(response.body, symbolize_names:true)
+      expect(body).to have_key(:errors)
+      expect(body[:errors]).to eq("A valid location must be provided")
+    end
+  end
+end
