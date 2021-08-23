46e19030242d5b151bbb4f30e9762156939d55bf
Diana Buffone <diana.pamelaet@gmail.com>
2021-08-23 14:07:11 -0400

    Create test for stations index request with happy and sad paths
    Co-authored-by: Alexander Brueck <73265236+brueck1988@users.noreply.github.com>

--- spec/requests/api/v1/stations/index_spec.rb
+++ spec/requests/api/v1/stations/index_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe "Search for stations by location" do
   describe "Happy Path" do
     it "Enpoint exists and has attributes" do
       location = "Denver, CO"
-      get "/api/v1/stations?location=#{location}"  
+      get "/api/v1/stations?location=#{location}"
       expect(response).to be_successful
-      
+
       body = JSON.parse(response.body, symbolize_names:true)
       expect(body).to have_key(:data)
-      expect(body[:data]).to have_key(:id)
-      expect(body[:data]).to have_key(:type)
-      expect(body[:data]).to have_key(:attributes)
-      expect(body[:data][:id]).to eq(nil)
-      expect(body[:data][:type]).to be_a String
-      expect(body[:data][:attributes]).to be_an Array
-      expect(body[:data][:attributes].size).to be < 21
-      
-      station_1 = body[:data][:attributes][0]
-      
+      expect(body[:data][0]).to have_key(:id)
+      expect(body[:data][0]).to have_key(:type)
+      expect(body[:data][0]).to have_key(:attributes)
+      expect(body[:data][0][:id]).to eq(nil)
+      expect(body[:data][0][:type]).to be_a String
+      expect(body[:data][0][:attributes]).to be_a Hash
+      expect(body[:data].size).to be < 21
+
+      station_1 = body[:data][0][:attributes]
+
       expect(station_1).to be_a Hash
       expect(station_1.size).to eq(9)
       expect(station_1).to have_key(:name)
@@ -29,38 +29,38 @@ RSpec.describe "Search for stations by location" do
       expect(station_1).to have_key(:street_address)
       expect(station_1).to have_key(:city)
       expect(station_1).to have_key(:state)
-      expect(station_1).to have_key(:zip_code)     
+      expect(station_1).to have_key(:zip_code)
     end
   end
-  
+
   describe "Sad Path/Edge Cases" do
     it "location is not provided" do
-      get "/api/v1/stations"  
+      get "/api/v1/stations"
       expect(response).to_not be_successful
       expect(response.status).to eq(400)
-      
+
       body = JSON.parse(response.body, symbolize_names:true)
       expect(body).to have_key(:errors)
       expect(body[:errors]).to eq("A valid location must be provided")
     end
-    
+
     it "location is blank" do
       location = " "
-      get "/api/v1/stations?location=#{location}"  
+      get "/api/v1/stations?location=#{location}"
       expect(response).to_not be_successful
       expect(response.status).to eq(400)
-      
+
       body = JSON.parse(response.body, symbolize_names:true)
       expect(body).to have_key(:errors)
       expect(body[:errors]).to eq("A valid location must be provided")
     end
-    
+
     it "location is invalid" do
       location = "oghkhhohoiho79808707"
-      get "/api/v1/stations?location=#{location}"  
+      get "/api/v1/stations?location=#{location}"
       expect(response).to_not be_successful
       expect(response.status).to eq(400)
-      
+
       body = JSON.parse(response.body, symbolize_names:true)
       expect(body).to have_key(:errors)
       expect(body[:errors]).to eq("A valid location must be provided")
