4ed93eb375debd95f1f78339301aa4d719a4281c
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-08-25 10:08:22 -0600

    Add error handling for sad path
    Co-authored-by: Diana Buffone 
    <71909590+Diana20920@users.noreply.github.com>

--- app/controllers/api/v1/stations_controller.rb
+++ app/controllers/api/v1/stations_controller.rb
@@ -5,5 +5,7 @@ class Api::V1::StationsController < ApplicationController
   def index
     stations = StationFacade.get_stations(params[:location])
     render json: StationSerializer.new(stations)
+  rescue NoMethodError
+    render json: { errors: "A valid location must be provided" }, status: :bad_request
   end
 end
--- app/facades/station_facade.rb
+++ app/facades/station_facade.rb
@@ -2,7 +2,7 @@ class StationFacade
   def self.get_stations(location)
     stations_data = StationService.get_stations(location)
     stations = stations_data[:fuel_stations]
-
+    
     stations.map do |station|
       Station.new(station)
     end
