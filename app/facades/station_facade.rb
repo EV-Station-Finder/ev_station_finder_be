b8e89d5e958b7f74277650f7794454abbb2bbc5c
Diana Buffone <diana.pamelaet@gmail.com>
2021-08-26 16:06:03 -0400

    Add get station method to station facade
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/facades/station_facade.rb
+++ app/facades/station_facade.rb
@@ -2,9 +2,18 @@ class StationFacade
   def self.get_stations(location)
     stations_data = StationService.get_stations(location)
     stations = stations_data[:fuel_stations]
-    
+
     stations.map do |station|
       Station.new(station)
     end
+  end
+
+  def self.get_station(id)
+    station_data = StationService.get_station(id)
+    latitude = station_data[:alt_fuel_station][:latitude]
+    longitude = station_data[:alt_fuel_station][:longitude]
+    weather_data = WeatherService.get_hourly_weather(latitude, longitude)
+
+    StationDetails.new(station_data, weather_data)
   end
 end
