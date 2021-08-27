06730fad2b3c872cb3de692c31610fa75525af5c
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-08-27 14:45:24 -0600

    Update initialize
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -5,10 +5,13 @@ class HourlyWeather
               :icon
 
   def initialize(weather_data)
-    # TODO: start here!
-    @time        = weather_data[]
-    @temperature = weather_data
-    @conditions  = weather_data
-    @icon        = weather_data
+    @time        = seconds_to_hour_minutes(weather_data[:dt])
+    @temperature = weather_data[:temp]
+    @conditions  = weather_data[:weather][0][:description]
+    @icon        = weather_data[:weather][0][:icon]
+  end
+  
+  def seconds_to_hour_minutes(seconds)
+    format("%02d:%02d:%02d", seconds / 3600, seconds / 60 % 60, seconds % 60)
   end
 end
