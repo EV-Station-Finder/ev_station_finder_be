8299201874dc88e6640579b8293df922376f39b1
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-08-31 14:01:54 -0600

    Work in progress
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- app/facades/station_facade.rb
+++ app/facades/station_facade.rb
@@ -10,9 +10,14 @@ class StationFacade
 
   def self.get_station(id)
     station_data = StationService.get_station(id)
-    latitude = station_data[:alt_fuel_station][:latitude]
-    longitude = station_data[:alt_fuel_station][:longitude]
-    weather_data = WeatherService.get_hourly_weather(latitude, longitude)
-    StationDetails.new(station_data[:alt_fuel_station], weather_data)
+    ##Consider refactoring
+    if station_data[:data].present?
+      latitude = station_data[:alt_fuel_station][:latitude]
+      longitude = station_data[:alt_fuel_station][:longitude]
+      weather_data = WeatherService.get_hourly_weather(latitude, longitude)
+      StationDetails.new(station_data[:alt_fuel_station], weather_data)
+    else
+      station_data
+    end
   end
 end
