2927d70087226760093d139c7de6b4f9d6917e92
Diana Buffone <diana.pamelaet@gmail.com>
2021-09-23 16:56:59 -0400

    Add method to account for nil ev_network in a single station
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/poros/station_details.rb
+++ app/poros/station_details.rb
@@ -4,6 +4,7 @@ class StationDetails
               :name,
               :status,
               :hours,
+              :ev_connector_types,
               :ev_network,
               :street_address,
               :city,
@@ -13,18 +14,19 @@ class StationDetails
               :hourly_weather
 
   def initialize(station_data, weather_data)
-    @id                = nil
-    @api_id            = station_data[:id]
-    @name              = station_data[:station_name]
-    @status            = status_finder(station_data[:status_code])
-    @hours             = station_data[:access_days_time]
-    @ev_network        = station_data[:ev_network]
-    @street_address    = station_data[:street_address]
-    @city              = station_data[:city]
-    @state             = station_data[:state]
-    @zip_code          = station_data[:zip]
-    @accepted_payments = format_payments(station_data)
-    @hourly_weather    = create_hourly_weather(weather_data)
+    @id                 = nil
+    @api_id             = station_data[:id]
+    @name               = station_data[:station_name]
+    @status             = status_finder(station_data[:status_code])
+    @hours              = station_data[:access_days_time]
+    @ev_connector_types = station_data[:ev_connector_types]
+    @ev_network         = set_ev_network(station_data[:ev_network])
+    @street_address     = station_data[:street_address]
+    @city               = station_data[:city]
+    @state              = station_data[:state]
+    @zip_code           = station_data[:zip]
+    @accepted_payments  = format_payments(station_data)
+    @hourly_weather     = create_hourly_weather(weather_data)
   end
 
   def status_finder(status_code)
@@ -40,7 +42,7 @@ class StationDetails
       'Status Unavailable'
     end
   end
-  
+
   def format_payments(station_data)
     accepted_payments = station_data[:cards_accepted]
     formatted_payments = []
@@ -53,7 +55,7 @@ class StationDetails
     end
     only_less_popular_payment_methods_available(formatted_payments)
   end
-  
+
   def rename_payment_methods(accepted_payments, formatted_payments)
     accepted_payment_codes = accepted_payments.split(" ")
     formatted_payments << "American Express" if accepted_payment_codes.include?("A")
@@ -64,15 +66,15 @@ class StationDetails
     formatted_payments << "Apple Pay" if accepted_payment_codes.include?("APPLE_PAY")
     formatted_payments << "Android Pay" if accepted_payment_codes.include?("ANDROID_PAY")
   end
-  
+
   def only_less_popular_payment_methods_available(formatted_payments)
     if formatted_payments.empty? #If defined payments are not available, but other payment forms are present
-      formatted_payments =["Payment Information Unavailable"] 
+      formatted_payments =["Payment Information Unavailable"]
     else
       formatted_payments
     end
   end
-  
+
   def create_hourly_weather(weather_data)
     current_and_hourly_weather = []
     current_and_hourly_weather << HourlyWeather.new(weather_data[:current])
@@ -80,5 +82,13 @@ class StationDetails
       current_and_hourly_weather << HourlyWeather.new(weather)
     end
     current_and_hourly_weather.first(10)
+  end
+
+  def set_ev_network(ev_network)
+    if ev_network.nil?
+      "Non-Networked"
+    else
+      ev_network
+    end
   end
 end
