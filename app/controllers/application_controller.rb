429aa476af7ee224d949519727e0d6c7e8efae3e
Diana Buffone <diana.pamelaet@gmail.com>
2021-10-01 17:44:43 -0400

    Remove unused rescue_from error handling
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::API
-  def welcome
-  end
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_bad_request
+  def welcome; end
 
   private
-
-  def record_not_found
-    render json: { errors: "No record found for given ID" }, status: :not_found
-  end
 
   def record_bad_request(exception)
     render json: { errors: exception.message }, status: :bad_request
