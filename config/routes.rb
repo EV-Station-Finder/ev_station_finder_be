6536ee90ac8b768cf25223f7d91ca4efb19e8605
Diana Buffone <diana.pamelaet@gmail.com>
2021-10-01 17:10:40 -0400

    Add route and controller for sessions to login a user
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :stations, only: [:index, :show]
       resources :users, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 end
