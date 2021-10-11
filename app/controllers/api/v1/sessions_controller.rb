a8222b8d9b1118983fff39e568dbe57c69118dc1
Diana Buffone <diana.pamelaet@gmail.com>
2021-10-11 19:48:55 -0400

    Implement user authorization with JWT
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,10 +3,20 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user&.authenticate(params[:password])
       token = JWT.encode({user_id: user.id}, 'hasselhoff', 'HS256')
-      # user_id = JWT.decode(token, 'hasselhoff', true, {algorithm: 'HS256'})
       render json: { data: { token: token, type: 'user' } }.to_json, status: 200
     else
       render json: { errors: "Invalid parameters" }, status: :bad_request
+    end
+  end
+
+  def authorize
+    # TODO: refactor
+    token = params[:token]
+    decoded_token = JWT.decode(token, 'hasselhoff', true, {algorithm: 'HS256'})
+    user_id = decoded_token[0]["user_id"]
+    user = User.find(user_id)
+    if user
+      render json: { data: { token: token, type: 'user' } }.to_json, status: 200
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :record_bad_request
+  rescue_from JWT::DecodeError, with: :not_logged_in
   def welcome; end
 
   private
 
   def record_bad_request(exception)
     render json: { errors: exception.message }, status: :bad_request
+  end
+
+  def not_logged_in
+    render json: { errors: "Not logged in" }, status: 401
   end
 end
