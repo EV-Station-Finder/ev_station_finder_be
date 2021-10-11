829589549f446d5450af3293cd2b0d4842bfaad7
Diana Buffone <diana.pamelaet@gmail.com>
2021-10-11 17:59:20 -0400

    Update session create to include a token in the response
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email].downcase)
     if user&.authenticate(params[:password])
-      render json: UserSerializer.new(user), status: 200
+      token = JWT.encode({user_id: user.id}, 'hasselhoff', 'HS256')
+      # user_id = JWT.decode(token, 'hasselhoff', true, {algorithm: 'HS256'})
+      render json: { data: { token: token, type: 'user' } }.to_json, status: 200
     else
       render json: { errors: "Invalid parameters" }, status: :bad_request
     end
