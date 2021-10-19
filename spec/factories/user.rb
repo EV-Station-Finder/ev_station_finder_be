0b30fbe64877acb1521e9fe8d34653e0fdf6ec2c
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-18 18:07:04 -0600

    Create user factory
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

---
+++ spec/factories/user.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Address.full_address }
+    street_address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    zip_code { Faker::Address.zip_code }
+    password { Faker::Internet.password }
+  end
+end
