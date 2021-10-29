8502a53107a2fa70275e013fa67d5bed80b0be0a
Diana Buffone <diana.pamelaet@gmail.com>
2021-10-29 12:27:48 -0400

    Change type from plural to singular
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/serializers/stations_serializer.rb
+++ app/serializers/stations_serializer.rb
@@ -1,4 +1,6 @@
 class StationsSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :distance, :status, :hours, :ev_network, :street_address, :city, :state, :zip_code
+
+  set_type :station
 end
