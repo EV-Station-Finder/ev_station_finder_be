5893875f437c606dbc11e47368092a465f5993ad
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-11-29 17:15:12 -0500

    Remove before syntax and instance variables
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'User creation' do
-  before do
     let(:user_params) { {
                     "first_name": "Hari",
                     "last_name": "Seldon",
@@ -14,11 +13,11 @@ RSpec.describe 'User creation' do
                       } }
     let(:headers) { {CONTENT_TYPE: "application/json",
                ACCEPT: "application/json"} }
-  end
+
 
   describe 'Happy Path' do
     it 'Creates a new user' do
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       new_user = User.last
       user_response = JSON.parse(response.body, symbolize_names: true)
@@ -33,7 +32,7 @@ RSpec.describe 'User creation' do
     end
 
     it 'Creates a token for a new user' do
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       new_user_id = User.last.id
       user_response = JSON.parse(response.body, symbolize_names: true)
@@ -47,8 +46,8 @@ RSpec.describe 'User creation' do
 
   describe 'Sad Path' do
     it 'Cannot create new user if email is not provided' do
-      @user_params[:"email"] = ""
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
+      user_params[:"email"] = ""
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       new_user = User.last
       user_response = JSON.parse(response.body, symbolize_names: true)
@@ -60,8 +59,8 @@ RSpec.describe 'User creation' do
 
     it 'Cannot create new user if email is already registered in database' do
 
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
-      @user_params2 = {
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+      user_params2 = {
                       "first_name": "Link",
                       "last_name": "Smith",
                       "email": "hari.seldon@foundation.com",
@@ -71,10 +70,10 @@ RSpec.describe 'User creation' do
                       "zip_code": "78968",
                       "password": "anotherverysecurepassword"
                      }
-      @headers = {CONTENT_TYPE: "application/json",
+      headers = {CONTENT_TYPE: "application/json",
                  ACCEPT: "application/json"}
 
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params2)
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params2)
       new_user = User.last
       user_response = JSON.parse(response.body, symbolize_names: true)
 
@@ -85,8 +84,8 @@ RSpec.describe 'User creation' do
     end
 
     it 'Cannot create new user if password is not provided' do
-      @user_params[:"password"] = ""
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
+      user_params[:"password"] = ""
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       new_user = User.last
       user_response = JSON.parse(response.body, symbolize_names: true)
@@ -97,8 +96,8 @@ RSpec.describe 'User creation' do
     end
 
     it 'Cannot create new user if street address is not provided' do
-      @user_params[:"street_address"] = ""
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
+      user_params[:"street_address"] = ""
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       new_user = User.last
       user_response = JSON.parse(response.body, symbolize_names: true)
@@ -109,8 +108,8 @@ RSpec.describe 'User creation' do
     end
 
     it 'Cannot create new user if city is not provided' do
-      @user_params[:"city"] = ""
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
+      user_params[:"city"] = ""
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       new_user = User.last
       user_response = JSON.parse(response.body, symbolize_names: true)
@@ -121,8 +120,8 @@ RSpec.describe 'User creation' do
     end
 
     it 'Cannot create new user if state is not provided' do
-      @user_params[:"state"] = ""
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
+      user_params[:"state"] = ""
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       new_user = User.last
       user_response = JSON.parse(response.body, symbolize_names: true)
@@ -133,8 +132,8 @@ RSpec.describe 'User creation' do
     end
 
     it 'Cannot create new user if zip code is not provided' do
-      @user_params[:"zip_code"] = ""
-      post "/api/v1/users", headers: @headers, params: JSON.generate(@user_params)
+      user_params[:"zip_code"] = ""
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       new_user = User.last
       user_response = JSON.parse(response.body, symbolize_names: true)
