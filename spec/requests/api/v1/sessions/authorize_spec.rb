260002b2e89f52643d4695ebb720f0f44b9855c8
Diana Buffone <diana.pamelaet@gmail.com>
2021-12-01 19:50:24 -0500

    Implement let blocks and clean up spec
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- spec/requests/api/v1/sessions/authorize_spec.rb
+++ spec/requests/api/v1/sessions/authorize_spec.rb
@@ -1,38 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'User authorization' do
+  let(:registration_params) { {
+                                "first_name": "Hari",
+                                "last_name": "Seldon",
+                                "email": "hari.seldon@foundation.com",
+                                "street_address": "123 Planet XYZ",
+                                "city": "Jupiter",
+                                "state": "UN",
+                                "zip_code": "12345",
+                                "password": "verysecurepassword"
+                                } }
+    let(:headers) { {CONTENT_TYPE: "application/json",
+                     ACCEPT: "application/json"} }
+    let(:login_params) { {email: "hari.seldon@foundation.com",
+                          password: "verysecurepassword"}}
+    let(:session_response) { JSON.parse(response.body, symbolize_names: true) }
+    let(:token)  { session_response[:data][:token] }
+    let(:token_params) { {"token": token} }
+    let(:incorrect_token_params) { {"token": "wrJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxNDV9.l_pdekbafUTPaz88h1qXUIw_Bg7PJPa_4Eu81AN7yfA"} }
+    let(:empty_token_params) { {"token": ""} }
+
   before do
-    registration_params = {
-                            "first_name": "Hari",
-                            "last_name": "Seldon",
-                            "email": "hari.seldon@foundation.com",
-                            "street_address": "123 Planet XYZ",
-                            "city": "Jupiter",
-                            "state": "UN",
-                            "zip_code": "12345",
-                            "password": "verysecurepassword"
-                           }
-
-    @headers = {CONTENT_TYPE: "application/json",
-               ACCEPT: "application/json"}
-
-    post "/api/v1/users", headers: @headers, params: JSON.generate(registration_params)
-
-    login_params = {
-                      email: "hari.seldon@foundation.com",
-                      password: "verysecurepassword"
-                    }
+    post "/api/v1/users", headers: headers, params: JSON.generate(registration_params)
     post "/api/v1/sessions", headers: headers, params: JSON.generate(login_params)
-
-    session_response = JSON.parse(response.body, symbolize_names: true)
-    @token = session_response[:data][:token]
   end
 
   describe 'Happy Path' do
     it 'Authorizes a user' do
-      token_params = {"token": @token}
-      get api_v1_authorize_path, headers: @headers, params: token_params
-      session_response = JSON.parse(response.body, symbolize_names: true)
+      get api_v1_authorize_path, headers: headers, params: token_params
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
@@ -46,28 +42,23 @@ RSpec.describe 'User authorization' do
 
   describe 'Sad Path' do
     it 'Does not authorize user because token is incorrect' do
-      token_params = {"token": "wrJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxNDV9.l_pdekbafUTPaz88h1qXUIw_Bg7PJPa_4Eu81AN7yfA"}
-      get api_v1_authorize_path, headers: @headers, params: token_params
-      session_response = JSON.parse(response.body, symbolize_names: true)
+      get api_v1_authorize_path, headers: headers, params: incorrect_token_params
 
       expect(response).to_not be_successful
       expect(response.status).to eq(401)
       expect(session_response[:errors]).to eq("Unauthorized")
     end
-    
+
     it 'Does not authorize user because token is empty string' do
-      token_params = {"token": ""}
-      get api_v1_authorize_path, headers: @headers, params: token_params
-      session_response = JSON.parse(response.body, symbolize_names: true)
+      get api_v1_authorize_path, headers: headers, params: empty_token_params
 
       expect(response).to_not be_successful
       expect(response.status).to eq(401)
       expect(session_response[:errors]).to eq("Unauthorized")
     end
-    
+
     it 'Does not authorize user because token is not provided' do
       get api_v1_authorize_path
-      session_response = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to_not be_successful
       expect(response.status).to eq(401)
