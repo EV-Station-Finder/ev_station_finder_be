d2e0efb6e6d40f79bea95457c8b1c1d7eb2c3abc
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-06 10:59:38 -0700

    Update spec
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- spec/requests/api/v1/users/update_spec.rb
+++ spec/requests/api/v1/users/update_spec.rb
@@ -11,7 +11,10 @@ RSpec.describe "Update user" do
                               password: 'verysecurepassword') }
   let(:user_record) { User.find(user1.id)  }
 
-  let!(:all_new_params) { { first_name: 'William',
+  let(:token1) { JWT.encode({user_id: user1.id}, 'hasselhoff', 'HS256') }
+  
+  let!(:all_new_params) { { token: token1,
+                            first_name: 'William',
                             last_name: 'Seldon Smith',
                             email: 'betteremail@example.com',
                             street_address: '3000 Virginia Beach Boulevard',
@@ -19,12 +22,12 @@ RSpec.describe "Update user" do
                             state: 'Maryland',
                             zip_code: '22304' } }
 
-  let!(:partially_new_params) { { email: 'workemail@example.com' } }
+  let!(:partially_new_params) { { token: token1, 
+                                  email: 'workemail@example.com' } }
 
-  let(:token1) { JWT.encode({user_id: user1.id}, 'hasselhoff', 'HS256') }
   let(:headers) { {CONTENT_TYPE: "application/json",
                   ACCEPT: "application/json"} }
-  let(:params1) { {token: token1} }
+
   let(:body) { JSON.parse(response.body, symbolize_names:true) }
 
   describe "Happy Path" do
@@ -37,44 +40,49 @@ RSpec.describe "Update user" do
       expect(body[:data]).to have_key(:id)
       expect(body[:data]).to have_key(:type)
       expect(body[:data]).to have_key(:attributes)
-      expect(body[:data][:attributes]).to_not eq(user1.first_name)
-      expect(body[:data][:attributes]).to eq(user_record.first_name)
-      expect(body[:data][:attributes]).to_not eq(user1.last_name)
-      expect(body[:data][:attributes]).to eq(user_record.last_name)
-      expect(body[:data][:attributes]).to_not eq(user1.email)
-      expect(body[:data][:attributes]).to eq(user_record.email)
-      expect(body[:data][:attributes]).to_not eq(user1.street_address)
-      expect(body[:data][:attributes]).to eq(user_record.street_address)
-      expect(body[:data][:attributes]).to_not eq(user1.city)
-      expect(body[:data][:attributes]).to eq(user_record.city)
-      expect(body[:data][:attributes]).to_not eq(user1.state)
-      expect(body[:data][:attributes]).to eq(user_record.state)
-      expect(body[:data][:attributes]).to_not eq(user1.zip_code)
-      expect(body[:data][:attributes]).to eq(user_record.zip_code)
+      expect(body[:data][:attributes][:first_name]).to_not eq(user1.first_name)
+      expect(body[:data][:attributes][:first_name]).to eq(user_record.first_name)
+      expect(body[:data][:attributes][:last_name]).to_not eq(user1.last_name)
+      expect(body[:data][:attributes][:last_name]).to eq(user_record.last_name)
+      expect(body[:data][:attributes][:email]).to_not eq(user1.email)
+      expect(body[:data][:attributes][:email]).to eq(user_record.email)
+      expect(body[:data][:attributes][:street_address]).to_not eq(user1.street_address)
+      expect(body[:data][:attributes][:street_address]).to eq(user_record.street_address)
+      expect(body[:data][:attributes][:city]).to_not eq(user1.city)
+      expect(body[:data][:attributes][:city]).to eq(user_record.city)
+      expect(body[:data][:attributes][:state]).to_not eq(user1.state)
+      expect(body[:data][:attributes][:state]).to eq(user_record.state)
+      expect(body[:data][:attributes][:zip_code]).to_not eq(user1.zip_code)
+      expect(body[:data][:attributes][:zip_code]).to eq(user_record.zip_code)
     end
+    
     it "Only user email attribute is updated", :vcr do
       patch "/api/v1/users", headers: headers, params: JSON.generate(partially_new_params)
 
       expect(response).to be_successful
       expect(response).to have_http_status(200)
-      expect(body[:data][:attributes]).to have_key(:email)
-      expect(body[:data][:attributes]).to_not have_key(:first_name)
-      expect(body[:data][:attributes]).to_not have_key(:last_name)
-      expect(body[:data][:attributes]).to_not have_key(:street_address)
-      expect(body[:data][:attributes]).to_not have_key(:city)
-      expect(body[:data][:attributes]).to_not have_key(:state)
-      expect(body[:data][:attributes]).to_not have_key(:zip_code)
+      expect(body).to have_key(:data)
+      expect(body[:data]).to have_key(:id)
+      expect(body[:data]).to have_key(:type)
+      expect(body[:data]).to have_key(:attributes)
+      expect(body[:data][:attributes][:first_name]).to eq(user_record.first_name)
+      expect(body[:data][:attributes][:last_name]).to eq(user_record.last_name)
       expect(body[:data][:attributes][:email]).to_not eq(user1.email)
       expect(body[:data][:attributes][:email]).to eq(user_record.email)
+      expect(body[:data][:attributes][:email]).to eq(partially_new_params[:email])
+      expect(body[:data][:attributes][:street_address]).to eq(user_record.street_address)
+      expect(body[:data][:attributes][:city]).to eq(user_record.city)
+      expect(body[:data][:attributes][:state]).to eq(user_record.state)
+      expect(body[:data][:attributes][:zip_code]).to eq(user_record.zip_code)
     end
   end
 
   describe "Sad Path/Edge Cases" do
-    xit "User does not exist", :vcr do
-      token2 = JWT.encode({user_id: 899}, 'hasselhoff', 'HS256')
+    it "User does not exist", :vcr do
+      token2 = JWT.encode({user_id: 99899}, 'hasselhoff', 'HS256')
       params2 = {token: token2}
 
-      patch "/api/v1/users", headers: headers, params: JSON.generate(partially_new_params)
+      patch "/api/v1/users", headers: headers, params: JSON.generate(params2)
 
       expect(response).to_not be_successful
       expect(response.status).to eq(404)
@@ -82,19 +90,18 @@ RSpec.describe "Update user" do
       expect(body[:errors]).to eq("User not found")
     end
 
-    xit "Token is invalid", :vcr do
+    it "Token is invalid", :vcr do
       altered_token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo1fQ.dU5lpMZtX69nehQPn0j23AApFaC8LW-dNuPSw9hH4cY"
       params2 = {token: altered_token}
 
-      patch "/api/v1/users", headers: headers, params: JSON.generate(partially_new_params)
-
+      patch "/api/v1/users", headers: headers, params: JSON.generate(params2)
       expect(response).to_not be_successful
       expect(response.status).to eq(401)
       expect(body).to have_key(:errors)
       expect(body[:errors]).to eq("Unauthorized")
     end
 
-    xit "Token is empty or not sent", :vcr do
+    it "Token is empty or not sent", :vcr do
       user2 =  User.create!(first_name: 'Bill',
                             last_name: 'Seldon',
                             email: 'email3@example.com',
@@ -105,7 +112,7 @@ RSpec.describe "Update user" do
                             password: 'verysecurepassword')
       params2 = {token: ""}
 
-      patch "/api/v1/users", headers: headers, params: JSON.generate(partially_new_params)
+      patch "/api/v1/users", headers: headers, params: JSON.generate(params2)
 
       expect(response).to_not be_successful
       expect(response.status).to eq(401)
