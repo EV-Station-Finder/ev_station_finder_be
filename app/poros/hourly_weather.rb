925e5ab9e91ce2d0d5d9616c33b7a42d55a653cf
Diana Buffone <diana.pamelaet@gmail.com>
2021-12-08 13:22:07 -0500

    Complete time format for station details weather
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -10,9 +10,8 @@ class HourlyWeather
     @conditions  = weather_data[:weather][0][:description]
     @icon        = weather_data[:weather][0][:icon]
   end
-  
-  def unix_time_to_hour_minutes(unix_time) # double check it's actually in Unix time
-    DateTime.strptime(unix_time.to_s, '%s').strftime("%H:%M")
-    #Time.at(seconds).strftime("%H:%M") # Alternate for local time - investigate later
+
+  def unix_time_to_hour_minutes(unix_time)
+    Time.at(unix_time.to_i).strftime("%H:%M %Z")
   end
 end
