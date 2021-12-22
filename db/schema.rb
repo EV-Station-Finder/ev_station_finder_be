cf9d9198af34d0e3306a1535b84cc32b74b55a84
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-21 17:08:33 -0700

    Add timestamps to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_235603) do
+ActiveRecord::Schema.define(version: 2021_12_22_000249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_12_21_235603) do
     t.string "state"
     t.string "zip_code"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "user_stations", "stations"
