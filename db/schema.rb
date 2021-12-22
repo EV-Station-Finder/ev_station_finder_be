f02cd6af3b52514406d8c8fa869f42085fbfa012
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-21 18:39:36 -0700

    Add migration to set default value of liked? to true for user_stations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_000249) do
+ActiveRecord::Schema.define(version: 2021_12_22_011536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_12_22_000249) do
     t.bigint "station_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "liked?"
+    t.boolean "liked?", default: true
     t.index ["station_id"], name: "index_user_stations_on_station_id"
     t.index ["user_id"], name: "index_user_stations_on_user_id"
   end
