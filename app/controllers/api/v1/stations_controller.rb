3c8eda0f32d4c13977bc53d10207127f8232f956
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-26 15:01:49 -0700

    Refactor action controller params

--- app/controllers/api/v1/stations_controller.rb
+++ app/controllers/api/v1/stations_controller.rb
@@ -1,29 +1,33 @@
 class Api::V1::StationsController < ApplicationController
   def index
-    stations = StationFacade.get_stations(search_location)
+# require "pry";binding.pry
+    stations = StationFacade.get_stations(search_location[:location])
     render json: StationsSerializer.new(stations)
   rescue NoMethodError
     render json: { errors: "A valid location must be provided" }, status: :bad_request
   end
 
   def show
-    station = StationFacade.get_station(station_api_id)
+    station = StationFacade.get_station(station_api_id[:id)
     if station.class == StationDetails
       render json: StationSerializer.new(station)
     else
-      render json: { errors: "Cannot find station with ID #{station_api_id}" }, status: :bad_request
+      render json: { errors: "Cannot find station with ID #{station_api_id[:id}" }, status: :bad_request
     end
   end
 
 private
 
   def station_api_id
-    api_id = params.permit(:id)
-    api_id[:id]
+    params.permit(:id)
   end
 
   def search_location
-    search_location = params.permit(:location)
-    search_location[:location]
+    params.permit(:location)
   end
+  
+  # def station_params
+  #   params.permit(:token, :api_id)
+  # end
 end
+
