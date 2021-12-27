47ecaffd8e79be54d242a0d38563d8d94ee6b579
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-27 14:59:14 -0700

    Add test for optional input

--- spec/facades/station_facade_spec.rb
+++ spec/facades/station_facade_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe StationFacade do
   describe "::get_stations" do
-    it "should return an array of station objects", :vcr do
-      location = "Denver, CO"
-      result = StationFacade.get_stations(location)
+    describe "provided with location but without user_id input" do
+      it "should return an array of station objects", :vcr do
+        location = "Denver, CO"
+        result = StationFacade.get_stations(location)
 
-      expect(result).to be_a(Array)
-      expect(result[0]).to be_a StationBasic
+        expect(result).to be_a(Array)
+        expect(result[0]).to be_a StationBasic
+      end
+    end
+    
+    describe "provided with location and user_id inputs" do
+      it "should return an array of station objects", :vcr do
+        location = "Denver, CO"
+        user_id = 1
+        result = StationFacade.get_stations(location, user_id)
+
+        expect(result).to be_a(Array)
+        expect(result[0]).to be_a StationBasic
+      end
     end
   end
 
