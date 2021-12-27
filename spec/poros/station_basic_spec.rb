a636865ac48b35838d6b8738a1f5759f81db7542
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-27 13:46:18 -0700

    Convert before_each to let block in existing tests

--- spec/poros/station_basic_spec.rb
+++ spec/poros/station_basic_spec.rb
@@ -2,76 +2,75 @@ require 'rails_helper'
 
 RSpec.describe StationBasic do
   describe "Station Object" do
-    before:each do
-      @incoming_hash = {:access_code=>"public",
-                       :access_days_time=>"24 hours daily",
-                       :access_detail_code=>nil,
-                       :cards_accepted=>nil,
-                       :date_last_confirmed=>"2021-08-25",
-                       :expected_date=>nil,
-                       :fuel_type_code=>"ELEC",
-                       :groups_with_access_code=>"Public",
-                       :id=>192187,
-                       :open_date=>"2021-07-04",
-                       :owner_type_code=>nil,
-                       :status_code=>"E",
-                       :station_name=>"Ideal Market Capitol Hill",
-                       :station_phone=>"877-455-3833",
-                       :updated_at=>"2021-08-25T01:13:53Z",
-                       :facility_type=>nil,
-                       :geocode_status=>"GPS",
-                       :latitude=>39.7332915,
-                       :longitude=>-104.9758502,
-                       :city=>"Denver",
-                       :intersection_directions=>nil,
-                       :plus4=>nil,
-                       :state=>"CO",
-                       :street_address=>"900 E 11th Ave",
-                       :zip=>"80218",
-                       :country=>"US",
-                       :bd_blends=>nil,
-                       :cng_dispenser_num=>nil,
-                       :cng_fill_type_code=>nil,
-                       :cng_psi=>nil,
-                       :cng_renewable_source=>nil,
-                       :cng_total_compression=>nil,
-                       :cng_total_storage=>nil,
-                       :cng_vehicle_class=>nil,
-                       :e85_blender_pump=>nil,
-                       :e85_other_ethanol_blends=>nil,
-                       :ev_connector_types=>["CHADEMO", "J1772COMBO"],
-                       :ev_dc_fast_num=>2,
-                       :ev_level1_evse_num=>nil,
-                       :ev_level2_evse_num=>nil,
-                       :ev_network=>"eVgo Network",
-                       :ev_network_web=>"https://www.evgo.com/",
-                       :ev_other_evse=>nil,
-                       :ev_pricing=>nil,
-                       :ev_renewable_source=>nil,
-                       :hy_is_retail=>nil,
-                       :hy_pressures=>nil,
-                       :hy_standards=>nil,
-                       :hy_status_link=>nil,
-                       :lng_renewable_source=>nil,
-                       :lng_vehicle_class=>nil,
-                       :lpg_primary=>nil,
-                       :lpg_nozzle_types=>nil,
-                       :ng_fill_type_code=>nil,
-                       :ng_psi=>nil,
-                       :ng_vehicle_class=>nil,
-                       :access_days_time_fr=>nil,
-                       :intersection_directions_fr=>nil,
-                       :bd_blends_fr=>nil,
-                       :groups_with_access_code_fr=>"Public",
-                       :ev_pricing_fr=>nil,
-                       :ev_network_ids=>{:station=>["27102"], :posts=>["56139", "56141"]},
-                       :distance=>0.59729,
-                       :distance_km=>0.96125}
-    end
+      let(:incoming_hash) { {:access_code=>"public",
+                             :access_days_time=>"24 hours daily",
+                             :access_detail_code=>nil,
+                             :cards_accepted=>nil,
+                             :date_last_confirmed=>"2021-08-25",
+                             :expected_date=>nil,
+                             :fuel_type_code=>"ELEC",
+                             :groups_with_access_code=>"Public",
+                             :id=>192187,
+                             :open_date=>"2021-07-04",
+                             :owner_type_code=>nil,
+                             :status_code=>"E",
+                             :station_name=>"Ideal Market Capitol Hill",
+                             :station_phone=>"877-455-3833",
+                             :updated_at=>"2021-08-25T01:13:53Z",
+                             :facility_type=>nil,
+                             :geocode_status=>"GPS",
+                             :latitude=>39.7332915,
+                             :longitude=>-104.9758502,
+                             :city=>"Denver",
+                             :intersection_directions=>nil,
+                             :plus4=>nil,
+                             :state=>"CO",
+                             :street_address=>"900 E 11th Ave",
+                             :zip=>"80218",
+                             :country=>"US",
+                             :bd_blends=>nil,
+                             :cng_dispenser_num=>nil,
+                             :cng_fill_type_code=>nil,
+                             :cng_psi=>nil,
+                             :cng_renewable_source=>nil,
+                             :cng_total_compression=>nil,
+                             :cng_total_storage=>nil,
+                             :cng_vehicle_class=>nil,
+                             :e85_blender_pump=>nil,
+                             :e85_other_ethanol_blends=>nil,
+                             :ev_connector_types=>["CHADEMO", "J1772COMBO"],
+                             :ev_dc_fast_num=>2,
+                             :ev_level1_evse_num=>nil,
+                             :ev_level2_evse_num=>nil,
+                             :ev_network=>"eVgo Network",
+                             :ev_network_web=>"https://www.evgo.com/",
+                             :ev_other_evse=>nil,
+                             :ev_pricing=>nil,
+                             :ev_renewable_source=>nil,
+                             :hy_is_retail=>nil,
+                             :hy_pressures=>nil,
+                             :hy_standards=>nil,
+                             :hy_status_link=>nil,
+                             :lng_renewable_source=>nil,
+                             :lng_vehicle_class=>nil,
+                             :lpg_primary=>nil,
+                             :lpg_nozzle_types=>nil,
+                             :ng_fill_type_code=>nil,
+                             :ng_psi=>nil,
+                             :ng_vehicle_class=>nil,
+                             :access_days_time_fr=>nil,
+                             :intersection_directions_fr=>nil,
+                             :bd_blends_fr=>nil,
+                             :groups_with_access_code_fr=>"Public",
+                             :ev_pricing_fr=>nil,
+                             :ev_network_ids=>{:station=>["27102"], 
+                                               :posts=>["56139", "56141"]},
+                             :distance=>0.59729,
+                             :distance_km=>0.96125} }
 
     describe "recieves station data but no user_id" do
       it "exists, has attributes, and Status is available" do
-        new_station = StationBasic.new(@incoming_hash)
+        new_station = StationBasic.new(incoming_hash)
 
         expect(new_station).to be_a StationBasic
         expect(new_station.id).to be_nil
@@ -88,26 +87,26 @@ RSpec.describe StationBasic do
       end
 
       it "Status is 'Coming Soon'" do
-        @incoming_hash[:status_code] = "P"
-        new_station = StationBasic.new(@incoming_hash)
+        incoming_hash[:status_code] = "P"
+        new_station = StationBasic.new(incoming_hash)
         expect(new_station.status).to eq("Coming Soon")
       end
 
       it "Status is 'Temporarily Closed'" do
-        @incoming_hash[:status_code] = "T"
-        new_station = StationBasic.new(@incoming_hash)
+        incoming_hash[:status_code] = "T"
+        new_station = StationBasic.new(incoming_hash)
         expect(new_station.status).to eq("Temporarily Closed")
       end
 
       it "Status is 'Status Unavailable'" do
-        @incoming_hash[:status_code] = ""
-        new_station = StationBasic.new(@incoming_hash)
+        incoming_hash[:status_code] = ""
+        new_station = StationBasic.new(incoming_hash)
         expect(new_station.status).to eq("Status Unavailable")
       end
 
       it "Account for when ev_network is nil/null" do
-        @incoming_hash[:ev_network] = nil
-        new_station = StationBasic.new(@incoming_hash)
+        incoming_hash[:ev_network] = nil
+        new_station = StationBasic.new(incoming_hash)
         expect(new_station.ev_network).to eq("Non-Networked")
       end
     end
