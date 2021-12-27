a50c1d531c69a11bf6f6e5566f9e1b00c4691a2e
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-27 14:53:45 -0700

    Update api_id to always return an integer to overcome discrepency between external api id being an integer and the api_id being saved as a string in the database

--- app/poros/station_basic.rb
+++ app/poros/station_basic.rb
@@ -14,7 +14,8 @@ class StationBasic
 
   def initialize(station_data, user_id=nil)
     @id             = nil
-    @api_id         = station_data[:id]
+    @api_id         = station_data[:id].to_i # external api provides api_id as integer
+                                             # but station api_id is saved as string in database
     @name           = station_data[:station_name]
     @distance       = station_data[:distance]
     @status         = status_finder(station_data[:status_code])
