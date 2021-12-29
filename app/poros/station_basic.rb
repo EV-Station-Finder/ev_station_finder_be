591e01cff62264588fe84b29a154b3565faa30e1
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-29 15:33:40 -0700

    Add invalid user sad path

--- app/poros/station_basic.rb
+++ app/poros/station_basic.rb
@@ -30,6 +30,7 @@ class StationBasic
 
   def station_favorited?(station_api_id, user_id)
     return "User token not provided" if user_id.nil?
+    User.find(user_id) # Check for users that do not exist to raise exception
     return false if (station = Station.find_by(api_id: station_api_id)).nil?
     if station && user_station = UserStation.find_by(station_id: station.id, user_id: user_id)
       user_station.favorited?
