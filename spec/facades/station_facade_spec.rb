4a2476de94b9a5f9959d8b1a541a5c89b08ea48a
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-29 16:01:13 -0700

    Add actual user so that user.id exists

--- spec/facades/station_facade_spec.rb
+++ spec/facades/station_facade_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe StationFacade do
+  let!(:user1) { User.create(first_name: 'Bill',
+                              last_name: 'Seldon',
+                              email: 'email@example.com',
+                              street_address: '2954 Virginia Beach Boulevard',
+                              city: 'Virginia Beach',
+                              state: 'Virginia',
+                              zip_code: '23452',
+                              password: 'verysecurepassword') }
+                              
   describe "::get_stations" do
     describe "provided with location but without user_id input" do
       it "should return an array of station objects", :vcr do
@@ -15,9 +24,8 @@ RSpec.describe StationFacade do
     describe "provided with location and user_id inputs" do
       it "should return an array of station objects", :vcr do
         location = "Denver, CO"
-        user_id = 1
-        result = StationFacade.get_stations(location, user_id)
-
+        result = StationFacade.get_stations(location, user1.id)
+        
         expect(result).to be_a(Array)
         expect(result[0]).to be_a StationBasic
       end
